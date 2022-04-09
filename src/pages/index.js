0bfc76bb041c8931733d92744d57bf237dad23b5
0x0110Y <59718138+0x0110Y@users.noreply.github.com>
2022-04-10 00:12:43 +0800

    coomit
    coomit

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/Layout";
+
+import "../styles/layout.css";
+import { useIntl } from "gatsby-plugin-intl";
+
+export default function Home() {
+  const intl = useIntl();
+  const locale = intl.locale !== "pt" ? `/${intl.locale}` : "";
+
+  return (
+    <Layout title={intl.formatMessage({ id: "home" })} className="home">
+      <section className="hero">
+        <div className="message">
+          <h1>Gatsby INTL</h1>
+          <p>{intl.formatMessage({ id: "slogan" })}</p>
+          <Link className="button" to={`${locale}/aticles`}>
+            {intl.formatMessage({ id: "Aticles" })}
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  );
+}
